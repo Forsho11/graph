c165f9311546ad97c83286112283e1df5eb66a98
Matheus <matheus200211@gmail.com>
2023-01-30 18:33:31 -0300

    Update of style. Adjust the borders and colors.

--- BgDefinition.js
+++ BgDefinition.js
@@ -1,7 +1,7 @@
 /* BackGround Plugin */
 
 const image = new Image();
-// image.src = 'image';
+//  image.src = 'bg2.jpg';
 
 const plugin = {
     id: 'BackgroundImage',
@@ -9,7 +9,7 @@ const plugin = {
         if (image.complete) {
             const ctx = chart.ctx;
             const { top, left, width, height } = chart.chartArea;
-            const x = left + width - image.width;
+            const x = left + width - image.width;   
             const y = top + height - image.height;
             ctx.drawImage(image, x, y);
         } else {
--- myCanvas.js
+++ myCanvas.js
@@ -18,7 +18,7 @@ var barColorsBorder = [
     'rgb(255, 159, 64)',
     'rgb(255, 205, 86)',
     'rgb(75, 192, 192)',
-    'rgb(54, 162, 23)',
+    'rgb(54, 162, 235)',
     'rgb(153, 102, 255)',
     'rgb(201, 203, 207)'];
 
@@ -34,15 +34,17 @@ const config = ({
                 type: "bar",
                 label: 'Dataset 1',
                 backgroundColor: barColors,
-                bordercolor: barColorsBorder,
+                borderColor: barColorsBorder,
                 data: yValues,
+                borderWidth: 1,
             },
             {
                 type: "line",
                 label: 'Dataset 2',
                 backgroundColor: barColors,
-                bordercolor: barColorsBorder,
+                borderColor: barColorsBorder,
                 data: expectedValues,
+                borderWidth: 1,
             },
 
         ]
